92ef866d1752cdb56978bc4d200e6f8e2ea9a71c
LarissonRivero <Larissonrivero2@gmail.com>
2023-09-09 19:05:34 -0300

    Inicializacion del Proyecto y Creacion del Navbar

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Navbar from './components/Navbar';
+
+
+
+function App() {
+  
+  return (
+    <h1>
+      <Navbar />
+    </h1>
+     
+  )
+}
+
+export default App
